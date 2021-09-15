Fix normalization issues on preprocessing

preprocessing/GenerateAndSaveData.py
@@ -7,8 +7,7 @@ from preprocessing.PreprocessData import get_preprocessed_data
 
 
 def generate_and_save_data(directory: str, sample_rate: int = 16000, len_song: int = 120, len_sample: float = 0.25,
-                trial_size: float = 0.6, val_size: float = 0.2, save_folder: str = 'data',
-                           save_norm: str = 'normalizer', seed: int = 1001) -> None:
+                trial_size: float = 0.6, val_size: float = 0.2, save_folder: str = 'data', seed: int = 1001) -> None:
     """
     Generate data from raw .wav files and save them in a given folder
 
@@ -19,7 +18,6 @@ def generate_and_save_data(directory: str, sample_rate: int = 16000, len_song: i
     :param trial_size: the size of trial data
     :param val_size: the size of validation data
     :param save_folder: name of directory where data should be saved
-    :param save_norm: name of directory where normalizers should be saved
     :param seed: seed
     """
 
@@ -39,26 +37,9 @@ def generate_and_save_data(directory: str, sample_rate: int = 16000, len_song: i
     X_val, y_val = get_preprocessed_data(val_files, sample_rate, len_song, len_sample)
     X_test, y_test = get_preprocessed_data(test_files, sample_rate, len_song, len_sample)
 
-    # Normalize data
-    x_transformer = Normalizer()
-    X_train = x_transformer.fit_transform(X_train)
-    X_val = x_transformer.transform(X_val)
-    X_test = x_transformer.transform(X_test)
-
-    y_transformer = Normalizer()
-    y_train = y_transformer.fit_transform(y_train)
-    y_val = y_transformer.transform(y_val)
-    y_test = y_transformer.transform(y_test)
-
     # Save data
     if not isdir(save_folder):
         mkdir(save_folder)
 
-    if not isdir(save_norm):
-        mkdir(save_norm)
-
     np.savez(join(save_folder, 'data.npz'), X_train=X_train, y_train=y_train,
              X_val=X_val, y_val=y_val, X_test=X_test, y_test=y_test)
-
-    dump(x_transformer, join(save_norm, 'x_normalizer.joblib'))
-    dump(y_transformer, join(save_norm, 'y_normalizer.joblib'))

preprocessing/GenerateFrequencyDomains.py
@@ -12,7 +12,7 @@ def get_frequency_domains(data: np.ndarray) -> np.ndarray:
     :return: numpy array containing the frequency domains of the song (real and imaginary parts are separated)
     """
 
-    freq_domains = fft(data)
+    freq_domains = fft(data, norm='forward')
     freq_domains_separated = np.empty(shape=data.shape[0] * 2, dtype=np.float64)
     freq_domains_separated[:data.shape[0]] = freq_domains.real
     freq_domains_separated[data.shape[0]:] = freq_domains.imag